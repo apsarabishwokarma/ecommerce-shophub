4fde39ab30fa839c69c2918039ba91219aa0441a
apsarabiswokarma <apsarabishwokarma7@gmail.com>
2024-03-27 21:24:50 +0545

    project init

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // function getData() {
+    //   fetch("https://fakestoreapi.com/products")
+    //     .then((res) => res.json())
+    //     .then((json) => {
+    //       setData(json);
+    //       setIsLoading(false);
+    //     }).catch((e)=>{});
+    //   console.log("run");
+    // }
+
+    async function getData() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const json = await res.json();
+        setData(json);
+      } catch (e) {
+        console.log("error");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? "Loading...." : "Done"}
+      {data?.map(({ id, title }) => (
+        <div>{title}</div>
+      ))}
+      <br />
+    </div>
+  );
+};
+
+export default App;
