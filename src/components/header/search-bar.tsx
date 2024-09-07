1bcf8bec8ef8d3524ed381db9dc1929b65a522de
apsarabishwokarma <apsarabk94@gmail.com>
2024-09-07 19:19:20 +0545

    searc-context in sesrchbar

--- src/components/header/search-bar.tsx
+++ src/components/header/search-bar.tsx
@@ -1,11 +1,16 @@
 import { CiSearch } from "react-icons/ci";
+import { useSearch } from "./search-context";
 
 export default function SearchBar() {
+  const { searchQuery, setSearchQuery } = useSearch();
+
   return (
     <>
       <div className="flex max-w-2xl w-full flex-1 items-center border border-gray-300 px-4 rounded-full">
         <input
           placeholder="Search product"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           className="flex-1 border-r px-4 py-2 focus:outline-none "
         />
         <button className="flex items-center px-4 py-2 ">
